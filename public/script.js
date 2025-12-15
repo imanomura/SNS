612c253adb3fb50743d256e4060590f01b138233
imanomura <206794822+imanomura@users.noreply.github.com>
2025-12-15 22:54:14 +0900

    post,フォームで画像を送る

--- public/script.js
+++ public/script.js
@@ -1,27 +1,5 @@
 'use strict';
-import { createApp } from 'https://unpkg.com/petite-vue?module';
-const file = document.querySelector('input').files[0];
 
-document.querySelector('button').addEventListener('click', fetchData);
-
-// creatApp({
-//   form_email: '',
-//   form_pass: '',
-//   form_BH: '',
-//   form_image: '',
-//   async submitData() {
-//     if (!this.form_email && !this.form_pass && !this.form_BH && !this.form_image) {
-//       alart('すべての項目を入力してください');
-//     }
-//     if (!this.form_email.includes('@')) {
-//       alart('メールアドレスを入力してください');
-//     };
-//     try{
-
-//     }
-//   },
-
-// });
 function Registraapp() {
   return {
     form_email: '',
@@ -29,11 +7,30 @@ function Registraapp() {
     form_BH: '',
     form_image: null,
     count: 0,
+    uploadImage: null,
+    data: null,
+    file: null,
 
+    // /AI
+    form_image(e) {
+      this.file = e.target.files[0];
+      this.uploadImage = URL.createObjectURL(this.file);
+    },
+    //Ai
+
+    //登録
     async submitData() {
-      const file = document.querySelector('input').files[0];
+      // const file = document.querySelector('input').files[0];
       if (!file) {
-        window.alert('画像ファイルを選択してください');
+        alert('画像ファイルを選択してください');
+        return;
+      }
+      if (!this.form_email && !this.form_pass && !this.form_BH && !this.form_image) {
+        alert('すべての項目を入力してください');
+        return;
+      }
+      if (!this.form_email.includes('@')) {
+        alert('メールアドレスを入力してください');
         return;
       }
       const form_Data = new FormData();
@@ -43,8 +40,8 @@ function Registraapp() {
       form_Data.append('new_pass', this.form_pass);
       form_Data.append('new_BH', this.form_BH);
       // form_Data.append('new_image', this.form_image);
-      form_Data.append('image', file);
-      form_Data.append('completed', false);
+      form_Data.append('image', this.file);
+      form_Data.append('completed', 'false');
       const url = 'https://httpbin.org/post';
       const res = await fetch(url, {
         method: 'POST',
@@ -52,31 +49,10 @@ function Registraapp() {
       });
       const obj = await res.json();
       this.data = obj;
-      const image = obj.files.image;
-      document.querySelector('(img').src = image;
+      // this.uploadImage = obj.files.image;
+      // document.querySelector('img').src = this.uploadImage;
       console.log(JSON.stringify(this.data, null, 2));
     }
   };
 }
-
-function dataset() {
-  const body = new FormData();
-  body.append('userId', 1);
-  body.append('titele', 'new_member');
-  body.append('new_email', 'new_member');
-  body.append('new_pass', 'new_member');
-  body.append('new_BH', 'new_member');
-  body.append('new_image', 'new_member');
-  body.append('completed,false');
-  return {
-    data: false,
-
-    async fetchData() {
-      const url = 'https://httpbin.org/post';
-      const res = await fetch(url);
-      const obj = await res.json();
-      this.data = obj;
-      console.log(JSON.stringify(this.data, null, 2));
-    }
-  };
-}
+//登録
